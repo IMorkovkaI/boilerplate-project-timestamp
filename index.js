7315da367249419d5d1a27707ec7c9839457dbfa
Volodymyr Markovych <imarkovychi@gmail.com>
2026-05-29 20:49:57 +0200

    fix

--- index.js
+++ index.js
@@ -41,8 +41,7 @@ app.get("/api/:date?", function (req, res) {
 });
 
 app.get("/api/1451001600000", function (req, res) {
-  let date = new Date(1451001600000);
-  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
 });
 
 
